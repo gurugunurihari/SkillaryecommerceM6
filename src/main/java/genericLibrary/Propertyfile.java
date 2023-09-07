56305918b25cd4e0265f547926518e8eeda60f81
gurug <gurug@LAPTOP-7M7UDB2K>
2023-09-07 11:05:40 +0530

    commit message

---
+++ src/main/java/genericLibrary/Propertyfile.java
@@ -0,0 +1,19 @@
+package genericLibrary;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.StringReader;
+import java.util.Properties;
+
+public class Propertyfile {
+
+	public String getPropertydata(String key) throws Throwable {
+	
+		Properties p=new Properties();
+		FileInputStream fis=new FileInputStream(IPathConstant.propertyfilepath);
+		p.load(fis);
+		return p.getProperty(key);
+
+	}
+
+}
---
+++ src/main/java/genericLibrary/photo.java
@@ -0,0 +1,10 @@
+package genericLibrary;
+
+public class photo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
